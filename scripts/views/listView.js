416c1f4f0fd9f31b4d37d1d0dfd82a8c2da8b23d
Sarah DeBey <sarah.debey@gmail.com>
2016-09-02 12:31:03 -0700

    adds styling and layout for mobile and desktop view

--- scripts/views/listView.js
+++ scripts/views/listView.js
@@ -12,17 +12,18 @@
     });
   };
 
-  listView.highlightMarket = function(market) {
-    $('.markets').css('background-color', '#C0D7EA');
-    $('#' + market.id).css('background-color', '#edf4f9');
-    //go to the a tag with href of id
-    // $('list-container').find('a').attr('href', '#' + market.id);
-  };
-
-  listView.unhighlightMarket = function(market) {
-    console.log('unhighlight');
-    $('.markets').css('background-color', '#C0D7EA');
-  };
+  // listView.highlightMarket = function(market) {
+  //   $('.markets').css('background-color', '#A2C3A4');
+  //   $('#' + market.id).css('background-color', '#C4F1BE');
+  //   //go to the a tag with href of id
+  //   // $('list-container').find('a').attr('href', '#' + market.id);
+  // };
+  //
+  // listView.unhighlightMarket = function(market) {
+  //   console.log('unhighlight');
+  //   $('.markets').css('background-color', '#A2C3A4');
+  //   $('.markets').css('hover', '#C4F1BE');
+  // };
 
   module.listView = listView;
 })(window);
